d93946a179337fdc1b3728e1ea5be97568db6e95
Jeremy Farnault <haskkor@gmail.com>
2022-03-08 10:58:23 +1300

    pluses and quiz

---
+++ pluses.py
@@ -0,0 +1,13 @@
+print("my name is robinou farnault ")
+
+print("ten plus 2345 is ")
+print(10 + 2345)    
+print("fifty plus nintyfive is")  
+print(50 + 95 )
+print("twohundred plus onehundred and sixteythree")
+print( 200 + 153 )
+
+print("the numberer of seconds in a year is ?") 
+
+print(60 * 60 * 24 * 365)
+
---
+++ quiz.py
@@ -0,0 +1,10 @@
+name = input("whats your name: ")
+print("hello buddy", name, "how are you")
+house = input("were do you live ")
+print("where in", house)
+
+question_number_1 = input("whats the capital of france ")
+if question_number_1 == "paris":
+    print("well done") 
+else:
+    print("sorry") 
