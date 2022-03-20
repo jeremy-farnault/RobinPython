5fd5da8ff883d078e8f451a29f7c85213c6a5822
Jeremy Farnault <haskkor@gmail.com>
2022-03-20 15:18:27 +1300

    javascript work

---
+++ javascript.js
@@ -0,0 +1,10 @@
+for(var n=1; n<10; n++)
+  document.write(n);
+
+function red(){
+  document.body.style.backgroundColor="red";
+}
+
+function blue(){
+  document.body.style.backgroundColor="blue";
+}
